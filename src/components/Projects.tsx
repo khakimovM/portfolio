8be650a58004361e7b50b23751394a643f74af4d
Khakimov Mukhammadaziz <khakimovmukhammadaziz@gmail.com>
2025-08-26 13:28:07 +0500

    projects added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,12 @@
 import ProjectItem from "./ui/ProjectItem";
 import Dashboard from "../assets/work-room-dashboard.png";
 import SignUp from "../assets/work-room-signup.png";
+import Weather1 from "../assets/weather1.png";
+import Weather2 from "../assets/weather2.png";
+import Test1 from "../assets/test1.png";
+import Test2 from "../assets/test2.png";
+import Test3 from "../assets/test3.png";
+import Timer from "../assets/timer.png";
 
 const myProjects = [
   {
@@ -10,6 +16,30 @@ const myProjects = [
       "This project is a workspace created for people to collaborate on their projects, to find employees, or to find a job for themselves.",
     frontLink: "https://github.com/khakimovM/work-room-main",
     backEndLink: "https://github.com/khakimovM/work-room-backend-main",
+  },
+  {
+    projectImg: [Weather1, Weather2],
+    name: "Wheather",
+    about:
+      "This small project will output the current weather forecast for the searched city and the next 5 days.It's link to enter this project: https://weather-delta-one-97.vercel.app/",
+    frontLink: "https://github.com/khakimovM/Weather",
+    backEndLink: null,
+  },
+  {
+    projectImg: [Test1, Test2, Test3],
+    name: "Solve and create tests with category",
+    about:
+      "In this small project, any user can create test categories and add options to them, developing them one by one.It's link to enter this project: https://solve-tests-category.vercel.app/",
+    frontLink: "https://github.com/khakimovM/solve-tests-category",
+    backEndLink: null,
+  },
+  {
+    projectImg: [Timer],
+    name: "Timer",
+    about:
+      "In this small project, any user can create test categories and add options to them, developing them one by one.It's link to enter this project: https://solve-tests-category.vercel.app/",
+    frontLink: "https://github.com/khakimovM/solve-tests-category",
+    backEndLink: null,
   },
 ];
 
--- src/components/ui/ProjectItem.tsx
+++ src/components/ui/ProjectItem.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "../../assets/styles/swiper.css";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-// import required modules
+
 import { Pagination } from "swiper/modules";
 interface Props {
-  projectImg: string[]; // ReactNode emas, string tipini ishlatish kerak
+  projectImg: string[];
   name: string;
   about: string;
   frontLink?: string | null;
@@ -23,7 +22,7 @@ const ProjectItem: React.FC<Props> = ({
   backEndLink,
 }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-6 bg-opacity-30 bg-cyan-100 border-blue-400 shadow-lg rounded-lg hover:scale-105 transition-transform">
+    <div className="flex flex-col items-center justify-between p-6 bg-opacity-30 bg-cyan-100 border-blue-400 shadow-lg rounded-lg hover:scale-105 transition-transform">
       <div className="w-fit h-fit max-w-[300px] max-h-[300px] flex items-center justify-center bg-white rounded-lg overflow-hidden">
         <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
           {projectImg.map((Image, index) => {
@@ -39,29 +38,28 @@ const ProjectItem: React.FC<Props> = ({
       <div className="mt-4 text-center">
         <h2 className="text-xl font-semibold text-white">{name}</h2>
         <p className="text-sm text-white opacity-80 mt-2">{about}</p>
-
-        <div className="flex gap-4 mt-4 justify-center w-full">
-          {frontLink && (
-            <a
-              href={frontLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="px-4 py-2 w-full text-sm bg-blue-700 text-white rounded-lg hover:bg-blue-800 transition duration-300"
-            >
-              Frontend
-            </a>
-          )}
-          {backEndLink && (
-            <a
-              href={backEndLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="px-4 py-2 w-full text-sm bg-green-700 text-white rounded-lg hover:bg-green-800 transition duration-300"
-            >
-              Backend
-            </a>
-          )}
-        </div>
+      </div>
+      <div className="flex gap-4 items-center text-center mt-4 justify-center w-full">
+        {frontLink && (
+          <a
+            href={frontLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-4 py-2 w-full text-sm bg-blue-700 text-white rounded-lg hover:bg-blue-800 transition duration-300"
+          >
+            Frontend
+          </a>
+        )}
+        {backEndLink && (
+          <a
+            href={backEndLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-4 py-2 w-full text-sm bg-green-700 text-white rounded-lg hover:bg-green-800 transition duration-300"
+          >
+            Backend
+          </a>
+        )}
       </div>
     </div>
   );
